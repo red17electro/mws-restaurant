8ef0f3777110d88e28882d29fc7f7acc11a1896b
Server <red17electro@gmail.com>
2018-05-26 13:37:16 +0200

    fix: change the image tag for figure tag and adjust styles.css

--- js/main.js
+++ js/main.js
@@ -153,29 +153,34 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const figure = document.createElement('figure');
+  const figCaption = document.createElement('figcaption');
 
   const image = document.createElement('img');
-  image.className = 'restaurant-img';
+  figure.className = 'restaurant-img';
   image.alt = DBHelper.altTextForTheImageOfTheRestaurant(restaurant);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  figure.append(image);
+  figure.append(figCaption);
+
+  li.append(figure);
 
   const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  figCaption.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  figCaption.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  figCaption.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  figCaption.append(more)
 
   return li
 }
