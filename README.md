590dea4a23a01228423a2d977fbce3a3ca1820dd
Server <red17electro@gmail.com>
2018-08-23 02:09:02 +0200

    refactor: change the README.md and remove actual Google Maps API key;

--- README.md
+++ README.md
@@ -6,11 +6,13 @@
 
 Please, follow the next steps in order to run the project.
 
-1. First of all, you need to start the server from [this](https://github.com/red17electro/mws-restaurant-stage-3) repository (**!important)**.
+1. First of all, for the files `index.html` and `restaurant.html`, change the `YOUR_GOOGLE_MAP_API_KEY` to your actual Google Maps API Key.
+
+2. Then you need to start the server from [this](https://github.com/red17electro/mws-restaurant-stage-3) repository (**!important)**.
 
    To do that, please run the command `node server` in the root directory of it.
 
-2. Now, once your server is set up, you can run the actual project. As it is under gulp, the latest build is located in the directory `dist`. Therefore, start up a simple HTTP server to serve up the site files on your local computer. Python has some simple tools to do this, and you don't even need to know Python. For most people, it's already installed on your computer. 
+3. Now, once your server is set up, you can run the actual project. As it is under gulp, the latest build is located in the directory `dist`. Therefore, start up a simple HTTP server to serve up the site files on your local computer. Python has some simple tools to do this, and you don't even need to know Python. For most people, it's already installed on your computer. 
 
 In a terminal, check the version of Python you have: `python -V`. If you have Python 2.x, spin up the server with `python -m SimpleHTTPServer 8887` (or some other port, if port 8887 is already in use.) For Python 3.x, you can use `python3 -m http.server 8887`. If you don't have Python installed, navigate to Python's [website](https://www.python.org/) to download and install the software.
 
--- sw.js
+++ sw.js
@@ -1,103 +1,109 @@
 /*jshint esversion: 6 */
 
-var CACHES_NAME = 'restaurant-v13';
+var CACHES_NAME = "restaurant-v13";
 
-self.addEventListener('install', function (event) {
-    var urlsToCache = [
-        '/',
-        '/index.html',
-        '/restaurant.html',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/js/idb.js',
-        '/js/restaurant_info.js',
-        '/img/1.webp',
-        '/img/2.webp',
-        '/img/3.webp',
-        '/img/4.webp',
-        '/img/5.webp',
-        '/img/6.webp',
-        '/img/7.webp',
-        '/img/8.webp',
-        '/img/9.webp',
-        '/img/10.webp',
-        '/img/No_image.svg',
-        '/restaurant.html?id=1',
-        '/restaurant.html?id=2',
-        '/restaurant.html?id=3',
-        '/restaurant.html?id=4',
-        '/restaurant.html?id=5',
-        '/restaurant.html?id=6',
-        '/restaurant.html?id=7',
-        '/restaurant.html?id=8',
-        '/restaurant.html?id=9',
-        '/restaurant.html?id=10'
-    ];
+self.addEventListener("install", function(event) {
+  var urlsToCache = [
+    "/",
+    "/index.html",
+    "/restaurant.html",
+    "/js/dbhelper.js",
+    "/js/main.js",
+    "/js/idb.js",
+    "/js/restaurant_info.js",
+    "/img/1.webp",
+    "/img/2.webp",
+    "/img/3.webp",
+    "/img/4.webp",
+    "/img/5.webp",
+    "/img/6.webp",
+    "/img/7.webp",
+    "/img/8.webp",
+    "/img/9.webp",
+    "/img/10.webp",
+    "/img/No_image.svg",
+    "/restaurant.html?id=1",
+    "/restaurant.html?id=2",
+    "/restaurant.html?id=3",
+    "/restaurant.html?id=4",
+    "/restaurant.html?id=5",
+    "/restaurant.html?id=6",
+    "/restaurant.html?id=7",
+    "/restaurant.html?id=8",
+    "/restaurant.html?id=9",
+    "/restaurant.html?id=10"
+  ];
 
-    event.waitUntil(
-        caches.open(CACHES_NAME).then(function (cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHES_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) return response;
-            return fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener('sync', function (event) {
-    if (event.tag === 'syncReviews') {
-        event.waitUntil(pushReviewsToTheServer());
-    }
+self.addEventListener("sync", function(event) {
+  if (event.tag === "syncReviews") {
+    event.waitUntil(pushReviewsToTheServer());
+  }
 });
 
 function pushReviewsToTheServer() {
-    if (typeof idb === "undefined" || typeof DBHelper === "undefined") {
-        self.importScripts('js/dbhelper.js', 'js/idb.js');
-    }
+  if (typeof idb === "undefined" || typeof DBHelper === "undefined") {
+    self.importScripts("js/dbhelper.js", "js/idb.js");
+  }
 
-    let promiseArray = [];
-    DBHelper.getDB();
-    DBHelper.restaurantDBPromise.then(function (db) {
-        if (!db) return;
+  let promiseArray = [];
+  DBHelper.getDB();
+  DBHelper.restaurantDBPromise
+    .then(function(db) {
+      if (!db) return;
 
-        var tx = db.transaction('restaurants', 'readwrite');
-        var store = tx.objectStore('restaurants');
+      var tx = db.transaction("restaurants", "readwrite");
+      var store = tx.objectStore("restaurants");
 
-        return store.openCursor();
-    }).then(function addReview(cursor) {
+      return store.openCursor();
+    })
+    .then(function addReview(cursor) {
+      if (!cursor) return;
 
-        if (!cursor) return;
+      var restaurant = cursor.value;
+      var reviews = restaurant.reviews;
 
-        var restaurant = cursor.value;
-        var reviews = restaurant.reviews;
+      if (reviews) {
+        reviews.forEach(element => {
+          if (element.offline) {
+            delete element.offline;
+            promiseArray.push(
+              fetch(`${DBHelper.SERVER_URL}/reviews/`, {
+                method: "post",
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(element)
+              }).then(response => response.json())
+            );
+          }
+        });
+      }
 
-        if (reviews) {
-            reviews.forEach(element => {
-                if (element.offline) {
-                    delete element.offline;
-                    promiseArray.push(fetch(`${DBHelper.SERVER_URL}/reviews/`, {
-                        method: 'post',
-                        headers: {
-                            "Content-Type": "application/json; charset=utf-8",
-                        },
-                        body: JSON.stringify(element)
-                    }).then(response => response.json()));
-                }
-            });
-        }
-
-        return cursor.continue().then(addReview);
-    }).then(function () {
-        return Promise.all(promiseArray).then(function () {
-            console.log(`Success! Promise all`);
-        }).catch(function (error) {
-            throw 'Silenced Exception! ' + error;
+      return cursor.continue().then(addReview);
+    })
+    .then(function() {
+      return Promise.all(promiseArray)
+        .then(function() {
+          console.log(`Success! Promise all`);
+        })
+        .catch(function(error) {
+          throw "Silenced Exception! " + error;
         });
     });
 }
